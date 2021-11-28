8eefaa7407dcaa12e266ea71b435b925457dcb03
koinuma1989 <33419371+koinuma1989@users.noreply.github.com>
2021-11-28 15:31:33 +0900

    s

--- Assets/Script/GameManeger.cs
+++ Assets/Script/GameManeger.cs
@@ -73,7 +73,7 @@ public class GameManeger : MonoBehaviour
         if (UIManegerScript.ValidatePlayerNameInput(playerNameList))
         {
             UIManegerScript.ClosePlayerNameInputPanel();
-        };
+        }
 
     }
 
--- Assets/Script/KomaSentaku.cs
+++ Assets/Script/KomaSentaku.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class KomaSentaku : MonoBehaviour
 {
@@ -8,43 +10,75 @@ public class KomaSentaku : MonoBehaviour
     private GameManeger gameManegerScript;
     GameObject clickedGameObject;
 
+    public GameObject namePlate;
+    public Text sentakuTitle;
+    private string nowPlayerName;
+
+
+    private int sentakuBanCount = -1;
+
     public GameObject[] komaObjList = new GameObject[7];
 
 
     private void Start()
     {
-        //gameManegerScript = gameManeger.GetComponent<GameManeger>();
+        gameManegerScript = gameManeger.GetComponent<GameManeger>();
     }
 
     //選択中の駒をくるくるさせる
     public void KomaKuru()
     {
+        clickedGameObject = null;
 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            clickedGameObject = hit.collider.gameObject;
+
+            NamePlateView(clickedGameObject.transform.position);
+
+
+            foreach (GameObject koma in komaObjList)
+            {
+                koma.GetComponent<SentakuSareruKoma>().selected = 0; //全部の駒の回転フラグをオフ
+                koma.transform.rotation = Quaternion.Euler(-90f, 180f, 0f);
+            }
+            clickedGameObject.GetComponent<SentakuSareruKoma>().selected = 1; //選択された駒の回転フラグをオン
+        }
     }
+
+    //選択する番を進める
+    public void NowPlayerName()
+    {
+        sentakuBanCount = sentakuBanCount + 1;
+        Debug.Log(sentakuBanCount);
+
+        nowPlayerName = gameManegerScript.playerNameList[sentakuBanCount];
+    }
+
+    //タイトル
+    public void TitleView()
+    {
+        sentakuTitle.text = nowPlayerName + "さんの駒を選択してください";
+    }
+
+    //現在の駒選択者の名前を選択した駒の下に表示
+    public void NamePlateView(Vector3 pos)
+    {
+        namePlate.GetComponent<Text>().text = nowPlayerName;
+        namePlate.transform.position = pos;
+    }
+
     void Update()
     {
-
+        TitleView();
 
         if (Input.GetMouseButtonDown(0))
         {
-
-            clickedGameObject = null;
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                clickedGameObject = hit.collider.gameObject;
-                Debug.Log(clickedGameObject);
-
-                foreach (GameObject koma in komaObjList)
-                {
-                    koma.GetComponent<SentakuSareruKoma>().selected = 0; //全部の駒の回転フラグをオフ
-                    koma.transform.rotation = Quaternion.Euler(-90f, 180f, 0f);
-                }
-                clickedGameObject.GetComponent<SentakuSareruKoma>().selected = 1; //選択された駒の回転フラグをオン
-            }
+            TitleView();
+            KomaKuru();
             
         }
     }
